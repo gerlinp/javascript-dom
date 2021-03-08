eb9935d74290f9ca6a2b8a4279249f5e40c36f15
gerlinp <gerlin.pierrelouis@gmail.com>
2021-03-07 22:17:35 -0500

    removing nodes

--- 3-making-changes-to-the-DOM/3-making-changes-to-the-DOM/vid-1-text-content-and-inner-html/start/app.js
+++ 3-making-changes-to-the-DOM/3-making-changes-to-the-DOM/vid-1-text-content-and-inner-html/start/app.js
@@ -6,6 +6,7 @@ const descriptionP = document.querySelector('p.description');
 const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
+const removeItemButton = document.querySelector('button.removeItemButton');
 
 toggleList.addEventListener('click', () => {
     if (listDiv.style.display == 'none') {
@@ -19,10 +20,19 @@ toggleList.addEventListener('click', () => {
 
 descriptionButton.addEventListener('click', () => {
     descriptionP.innerHTML = descriptionInput.value + ':';
+    descriptionInput.value = '';
 });
 
 addItemButton.addEventListener('click', () => {
+    let ul = document.getElementsByTagName('ul')[0];
     let li = document.createElement('li');
     li.textContent = addItemInput.value;
+    ul.appendChild(li);
+    addItemInput.value = '';
 });
 
+removeItemButton.addEventListener('click', () => {
+    let ul = document.getElementsByTagName('ul')[0];
+    let li = document.querySelector('li:last-child');
+    ul.removeChild(li);
+});
